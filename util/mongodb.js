3d9382efd2b3036e235d4fe27fd99e11f0ae7020
Matt Harker <matt@theflux.co.uk>
2016-01-13 09:51:01 +0000

    Not having a good morning clearly...

--- util/mongodb.js
+++ util/mongodb.js
@@ -15,7 +15,6 @@ function connect() {
                if (err) {
                    reject(err);
                } else {
-                   singleton = db;
                    resolve(db);
                }
             });
