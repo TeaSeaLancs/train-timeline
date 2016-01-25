52c74321c44b1a0e749450cf2016621178037c4c
Matt Harker <matt@theflux.co.uk>
2016-01-25 16:59:59 +0000

    Set not put

--- util/update.js
+++ util/update.js
@@ -14,7 +14,7 @@ function queue(id, data, time) {
     time = time || DEFAULT_TIME;
     if (!updates.has(id)) {
         debug(`Queueing update for ${id} in ${time/1000} seconds`, data);
-        updates.put(id, {
+        updates.set(id, {
             promise: pause(time).then(() => data)
         });
     }
